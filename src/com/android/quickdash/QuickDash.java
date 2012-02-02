0a149be503c60049681eb8caac22e8fffa112a3c
xoomdev <xoomdevnet@gmail.com>
2012-02-02 17:58:18 -0600

    Cheat a bit on some button states, add toasts to all actions.

--- src/com/android/quickdash/QuickDash.java
+++ src/com/android/quickdash/QuickDash.java
@@ -3,6 +3,7 @@ package com.android.quickdash;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.ContentResolver;
 import android.content.DialogInterface;
@@ -165,40 +166,41 @@ public class QuickDash extends Activity implements OnDismissListener, OnClickLis
             LocationManager mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
             IPowerManager mPowerManager = IPowerManager.Stub.asInterface(ServiceManager.getService("power"));
 
-            /* get all the values */
-            mWifiApEnabled = wifiManager.getWifiApState();
-            mRingerMode = am.getRingerMode();
-            mNetworkMode = Settings.Secure.getInt(
-                    getContentResolver(),
-                    Settings.Secure.PREFERRED_NETWORK_MODE);
-            mBrightnessLevel = Settings.System.getInt(
-                    getContentResolver(),
-                    Settings.System.SCREEN_BRIGHTNESS);
+            /* set images based on values */
+            int wifiState = wifiManager.getWifiState();
+            switch (wifiState) {
+                case WifiManager.WIFI_STATE_ENABLED:
+                case WifiManager.WIFI_STATE_ENABLING:
+                    mWifiButton.setImageResource(R.drawable.wifi_on);
+                    break;
+                case WifiManager.WIFI_STATE_DISABLING:
+                case WifiManager.WIFI_STATE_DISABLED:
+                    mWifiButton.setImageResource(R.drawable.wifi_off);
+                    break;
+            }
+
             mRotationEnabled = (Settings.System.getInt(
                     getContentResolver(),
                     Settings.System.ACCELEROMETER_ROTATION, 0) == 1);
-            mAirplaneEnabled = (Settings.System.getInt(
-                    getContentResolver(),
-                    Settings.System.AIRPLANE_MODE_ON, 0) == 1);
-            mAutoBrightEnabled = (Settings.System.getInt(
-                    getContentResolver(),
-                    Settings.System.SCREEN_BRIGHTNESS_MODE, 0) == 1);
-            syncValue = ContentResolver.getMasterSyncAutomatically();
-
-            /* set images based on values */
-            mWifiButton.setImageResource(wifiManager.isWifiEnabled() ?
-                    R.drawable.wifi_on : R.drawable.wifi_off);
-
             mRotationButton.setImageResource(mRotationEnabled ?
                     R.drawable.auto_rotate_on : R.drawable.auto_rotate_off);
 
-            mTetherButton.setImageResource(mWifiApEnabled ==
-                      WifiManager.WIFI_AP_STATE_ENABLED ?
-                    R.drawable.tether_on : R.drawable.tether_off);
+            mWifiApEnabled = wifiManager.getWifiApState();
+            switch (mWifiApEnabled) {
+                case WifiManager.WIFI_AP_STATE_ENABLED:
+                case WifiManager.WIFI_AP_STATE_ENABLING:
+                    mTetherButton.setImageResource(R.drawable.tether_on);
+                    break;
+                case WifiManager.WIFI_AP_STATE_DISABLED:
+                case WifiManager.WIFI_AP_STATE_DISABLING:
+                    mTetherButton.setImageResource(R.drawable.tether_off);
+                    break;
+            }
 
             mDataButton.setImageResource(cm.getMobileDataEnabled() ?
                     R.drawable.data_on : R.drawable.data_off);
 
+            mRingerMode = am.getRingerMode();
             if (mRingerMode == AudioManager.RINGER_MODE_SILENT) {
                 mSoundButton.setImageResource(R.drawable.sound_off);
             } else if (mRingerMode == AudioManager.RINGER_MODE_VIBRATE) {
@@ -206,10 +208,16 @@ public class QuickDash extends Activity implements OnDismissListener, OnClickLis
             } else if (mRingerMode == AudioManager.RINGER_MODE_NORMAL) {
                 mSoundButton.setImageResource(R.drawable.sound_on);
             }
-         
+
+            mAirplaneEnabled = (Settings.System.getInt(
+                    getContentResolver(),
+                    Settings.System.AIRPLANE_MODE_ON, 0) == 1);
             mAirplaneButton.setImageResource(mAirplaneEnabled ?
                     R.drawable.airplane_on : R.drawable.airplane_off);
 
+            mNetworkMode = Settings.Secure.getInt(
+                    getContentResolver(),
+                    Settings.Secure.PREFERRED_NETWORK_MODE);
             mLteButton.setImageResource(mNetworkMode ==
                       Phone.NT_MODE_GLOBAL ?
                     R.drawable.lte_on : R.drawable.lte_off);
@@ -220,7 +228,13 @@ public class QuickDash extends Activity implements OnDismissListener, OnClickLis
             mGpsButton.setImageResource(mLocationManager
                     .isProviderEnabled(LocationManager.GPS_PROVIDER) ?
                     R.drawable.gps_on : R.drawable.gps_off);
-            
+
+            mBrightnessLevel = Settings.System.getInt(
+                    getContentResolver(),
+                    Settings.System.SCREEN_BRIGHTNESS);
+            mAutoBrightEnabled = (Settings.System.getInt(
+                    getContentResolver(),
+                    Settings.System.SCREEN_BRIGHTNESS_MODE, 0) == 1);
             if (mAutoBrightEnabled) {
                 mBrightnessButton.setImageResource(
                         R.drawable.brightness_auto);
@@ -232,6 +246,7 @@ public class QuickDash extends Activity implements OnDismissListener, OnClickLis
                         R.drawable.brightness_half);
             }
 
+            syncValue = ContentResolver.getMasterSyncAutomatically();
             mSyncButton.setImageResource(syncValue ?
                     R.drawable.sync_on : R.drawable.sync_off);
 
@@ -258,6 +273,7 @@ public class QuickDash extends Activity implements OnDismissListener, OnClickLis
         if (v == mWifiButton) {
             WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
             boolean wifiEnabled = wifiManager.isWifiEnabled();
+            Toast.makeText(this, !wifiEnabled ? "Enabling WiFi" : "Disabling WiFi", Toast.LENGTH_SHORT).show();
             wifiManager.setWifiEnabled(!wifiEnabled);
             updateButtonState();
         } else if (v == mTetherButton) {
@@ -272,41 +288,53 @@ public class QuickDash extends Activity implements OnDismissListener, OnClickLis
                 Intent intent = new Intent(WifiManager.WIFI_AP_STATE_CHANGED_ACTION);
                 intent.putExtra("state", true);
                 sendBroadcast(intent);
+                mTetherButton.setImageResource(R.drawable.tether_on);
+                Toast.makeText(this, "Enabling WiFi AP mode", Toast.LENGTH_SHORT).show();
             } else {
                 wifiManager.setWifiApEnabled(null, false);
                 Intent intent = new Intent(WifiManager.WIFI_AP_STATE_CHANGED_ACTION);
                 intent.putExtra("state", false);
                 sendBroadcast(intent);
-            }
-            updateButtonState();      
+                Toast.makeText(this, "Disabling WiFi AP mode", Toast.LENGTH_SHORT).show();
+                mTetherButton.setImageResource(R.drawable.tether_off);
+            }     
         } else if (v == mDataButton) {
             ConnectivityManager cm = (ConnectivityManager) getSystemService
                     (CONNECTIVITY_SERVICE);
 
             boolean dataEnabled = cm.getMobileDataEnabled();
             cm.setMobileDataEnabled(!dataEnabled);
-            updateButtonState();
+            if (!dataEnabled) {
+                mDataButton.setImageResource(R.drawable.data_on);
+                Toast.makeText(this, "Enabling mobile data", Toast.LENGTH_SHORT).show();
+            } else {
+                mDataButton.setImageResource(R.drawable.data_off);
+                Toast.makeText(this, "Disabling mobile data", Toast.LENGTH_SHORT).show();
+            }
         } else if (v == mSoundButton) {
             AudioManager am = (AudioManager) getSystemService(AUDIO_SERVICE);
-
             int mode = am.getRingerMode();
             if (mode == AudioManager.RINGER_MODE_SILENT) {
+                Toast.makeText(this, "Changing Ringer Mode: Vibrate", Toast.LENGTH_SHORT).show();
                 am.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
             } else if (mode == AudioManager.RINGER_MODE_VIBRATE) {
+                Toast.makeText(this, "Changing Ringer Mode: Normal", Toast.LENGTH_SHORT).show();
                 am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
             } else {
+                Toast.makeText(this, "Changing Ringer Mode: Silent", Toast.LENGTH_SHORT).show();
                 am.setRingerMode(AudioManager.RINGER_MODE_SILENT);
             }
             updateButtonState();        
         } else if (v == mRotationButton) {
             boolean autorotate = (Settings.System.getInt(getContentResolver(),
                         Settings.System.ACCELEROMETER_ROTATION, 0) != 0);
+            Toast.makeText(this, !autorotate ? "Enabling auto-rotation" : "Disabling auto-rotation", Toast.LENGTH_SHORT).show();
             Settings.System.putInt(getContentResolver(),
                         Settings.System.ACCELEROMETER_ROTATION, autorotate ?  0 : 1);
-            Toast.makeText(this, autorotate ? "Auto rotate enabled" : "Auto rotate disabled", Toast.LENGTH_SHORT).show();
-            finish();
+            updateButtonState();
         } else if (v == mAirplaneButton) {
             boolean airplaneEnabled = (Settings.System.getInt(getContentResolver(), Settings.System.AIRPLANE_MODE_ON, 0) == 1);
+            Toast.makeText(this, !airplaneEnabled ? "Enabling airplane mode" : "Disabling airplane mode", Toast.LENGTH_SHORT).show();
             Settings.System.putInt(getContentResolver(), Settings.System.AIRPLANE_MODE_ON, airplaneEnabled ? 0 : 1);
             Intent intent = new Intent(Intent.ACTION_AIRPLANE_MODE_CHANGED);
             intent.putExtra("state", !airplaneEnabled);
@@ -318,8 +346,10 @@ public class QuickDash extends Activity implements OnDismissListener, OnClickLis
                     Settings.Secure.PREFERRED_NETWORK_MODE);
                 TelephonyManager tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
                 if (network == Phone.NT_MODE_GLOBAL) {
+                    Toast.makeText(this, "Disabling LTE", Toast.LENGTH_SHORT).show();
                     tm.toggleLTE(false);
                 } else if (network == Phone.NT_MODE_CDMA) {
+                    Toast.makeText(this, "Enabling LTE", Toast.LENGTH_SHORT).show();
                     tm.toggleLTE(true);
                 }
             } catch (SettingNotFoundException e) {
@@ -329,15 +359,20 @@ public class QuickDash extends Activity implements OnDismissListener, OnClickLis
             BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
             if (!mBluetoothAdapter.isEnabled()) {
                 mBluetoothAdapter.enable();
+                Toast.makeText(this, "Enabling Bluetooth", Toast.LENGTH_SHORT).show();
+                mBluetoothButton.setImageResource(R.drawable.bt_on);
             } else {
                 mBluetoothAdapter.disable();
+                Toast.makeText(this, "Disabling Bluetooth", Toast.LENGTH_SHORT).show();
+                mBluetoothButton.setImageResource(R.drawable.bt_off);
             }
-            updateButtonState();
         } else if (v == mGpsButton) {
             LocationManager mLocationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
             if (!mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                Toast.makeText(this, "Enabling GPS", Toast.LENGTH_SHORT).show();
                 Settings.Secure.setLocationProviderEnabled(getContentResolver(), LocationManager.GPS_PROVIDER, true);
             } else {
+                Toast.makeText(this, "Disabling GPS", Toast.LENGTH_SHORT).show();
                 Settings.Secure.setLocationProviderEnabled(getContentResolver(), LocationManager.GPS_PROVIDER, false);
             }
             updateButtonState();
@@ -350,28 +385,36 @@ public class QuickDash extends Activity implements OnDismissListener, OnClickLis
                 if (brightnessMode == 1) {
                     Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS_MODE, 0);
                     Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, 10);
+                    Toast.makeText(this, "New brightness level: 10", Toast.LENGTH_SHORT).show();
                     newBrightnessLevel = 10;
                 } else {
                     if (brightnessLevel == 10) {
                         Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, 50);
+                        Toast.makeText(this, "New brightness level: 50", Toast.LENGTH_SHORT).show();
                         newBrightnessLevel = 50;
                     } else if (brightnessLevel == 50) {
                         Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, 100);
+                        Toast.makeText(this, "New brightness level: 100", Toast.LENGTH_SHORT).show();
                         newBrightnessLevel = 100;
                     } else if (brightnessLevel == 100) {
                         Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, 150);
+                        Toast.makeText(this, "New brightness level: 150", Toast.LENGTH_SHORT).show();
                         newBrightnessLevel = 150;
                     } else if (brightnessLevel == 150) {
                         Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, 200);
+                        Toast.makeText(this, "New brightness level: 200", Toast.LENGTH_SHORT).show();
                         newBrightnessLevel = 200;
                     } else if (brightnessLevel == 200) {
                         Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, 255);
+                        Toast.makeText(this, "New brightness level: MAX", Toast.LENGTH_SHORT).show();
                         newBrightnessLevel = 255;
                     } else if (brightnessLevel == 255) {
                         Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS_MODE, 1);
+                        Toast.makeText(this, "New brightness level: 10", Toast.LENGTH_SHORT).show();
                         newBrightnessLevel = 10;
                     } else {
                         Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, 10);
+                        Toast.makeText(this, "New brightness level: 10", Toast.LENGTH_SHORT).show();
                         newBrightnessLevel = 10;
                     }
                 }
@@ -388,8 +431,10 @@ public class QuickDash extends Activity implements OnDismissListener, OnClickLis
         } else if (v == mSyncButton) {
             boolean syncMode = ContentResolver.getMasterSyncAutomatically();
             if (syncMode) {
+                Toast.makeText(this, "Disabling Sync", Toast.LENGTH_SHORT).show();
                 ContentResolver.setMasterSyncAutomatically(false);
             } else {
+                Toast.makeText(this, "Enabling Sync", Toast.LENGTH_SHORT).show();
                 ContentResolver.setMasterSyncAutomatically(true);
             }
             updateButtonState();
